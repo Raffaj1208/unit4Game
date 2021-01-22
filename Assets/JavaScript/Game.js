3e6c3a67a66626d94815f0b4cd789531620b10be
Raffaj1208 <raffa.codedrive@gmail.com>
2021-01-21 16:09:01 -0800

    buttons are functioning

--- Assets/JavaScript/Game.js
+++ Assets/JavaScript/Game.js
@@ -1,7 +1,11 @@
 var usrNumber = 0;
 var wins = 0;
 var losses = 0;
-var compScore = Math.floor(Math.random() * 100 + 5);
+var compScore = Math.floor(Math.random() * 500 + 5);
+var value1 = Math.floor(Math.random() * 100 + 5);
+var value2 = Math.floor(Math.random() * 100 + 5);
+var value3 = Math.floor(Math.random() * 100 + 5);
+var value4 = Math.floor(Math.random() * 100 + 5);
 
 function start() {
     $("#Wins").append(wins);
@@ -49,3 +53,28 @@ function matchRules() {
         update();
     }
 };
+matchRules();
+
+$('#btn1').on('click', function() {
+    usrNumber += value1;
+    $('#usrNumber').text(usrNumber);
+    console.log(value1);
+});
+
+$('#btn2').on('click', function() {
+    usrNumber += value2;
+    $('#usrNumber').text(usrNumber);
+    console.log(value2);
+});
+
+$('#btn3').on('click', function() {
+    usrNumber += value3;
+    $('#usrNumber').text(usrNumber);
+    console.log(value3);
+});
+
+$('#btn4').on('click', function() {
+    usrNumber += value4;
+    $('#usrNumber').text(usrNumber);
+    console.log(value4);
+});
